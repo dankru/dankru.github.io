8e3c5f82d1e74051152c5deffc25f1f82d2068dc
Neutral <pirogovde@mer.ci.nsu.ru>
2022-11-02 15:45:28 +0300

    made mobile version

--- span/js/script.js
+++ span/js/script.js
@@ -3,11 +3,28 @@
 $(document).ready(function () {
   $('.films__slider').slick({
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $('.events__slider').slick({
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $('.header__burger').click(function (event) {
     $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
   });
 });
